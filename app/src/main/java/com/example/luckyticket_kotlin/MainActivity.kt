babbc11440b5237d39f7881ebb7cae3cd2a3d062
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2021-09-03 15:43:57 +0300

    second commit

--- app/src/main/java/com/example/luckyticket_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/luckyticket_kotlin/MainActivity.kt
@@ -14,38 +14,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.check_button).setOnClickListener {
+        findViewById<Button>(R.id.btnCheckNumber).setOnClickListener {
             checkIsLucky()
         }
     }
-    @SuppressLint("CutPasteId")
-    private fun checkIsLucky(){
-        val lampImage= findViewById<ImageView>(R.id.lamp_image)
-        val inputNumber= findViewById<EditText>(R.id.ticket_number_editText).text
-        if(inputNumber.length<6) {
-            findViewById<EditText>(R.id.ticket_number_editText).error= getString(R.string.errorMessage)
 
-        }
-        else {
-            val leftPart=inputNumber.substring(0,3).toList().sumOf {  "$it".toInt()  }
-            val rightPart=inputNumber.substring(3,6).toList().sumOf {  "$it".toInt()  }
-            if(leftPart==rightPart) {
+    @SuppressLint("CutPasteId")
+    private fun checkIsLucky() {
+        val lampImage = findViewById<ImageView>(R.id.imgLamp)
+        val inputNumber = findViewById<EditText>(R.id.etTicketNumber).text
+        if (inputNumber.length < 6) {
+            findViewById<EditText>(R.id.etTicketNumber).error = getString(R.string.errorMessage)
+
+        } else {
+            val leftPart = inputNumber.substring(0, 3).toList().sumOf { "$it".toInt() }
+            val rightPart = inputNumber.substring(3, 6).toList().sumOf { "$it".toInt() }
+            if (leftPart == rightPart) {
                 Handler(Looper.getMainLooper()).postDelayed(
                     {
                         lampImage.setImageResource(R.drawable.gray_lamp_image)
                     },
-                    5000)
+                    5000
+                )
                 lampImage.setImageResource(R.drawable.green_lamp_image)
-                Toast.makeText(this, "That's a lucky ticket!",Toast.LENGTH_LONG).show()
-            }
-            else {
+                Toast.makeText(this, getString(R.string.toastMessageLucky), Toast.LENGTH_LONG)
+                    .show()
+            } else {
                 Handler(Looper.getMainLooper()).postDelayed(
                     {
                         lampImage.setImageResource(R.drawable.gray_lamp_image)
                     },
-                    5000)
+                    5000
+                )
                 lampImage.setImageResource(R.drawable.red_lamp_image)
-                Toast.makeText(this, "That's not a lucky ticket(",Toast.LENGTH_LONG).show()
+                Toast.makeText(this, getString(R.string.toastMessageNotLucky), Toast.LENGTH_LONG)
+                    .show()
             }
         }
     }
